75efd3f344cfd59933f78e33790634a64c47b5c4
mordylka420 <juliemordyl@gmail.com>
2025-05-12 00:23:11 +0200

    wip: new function: remove user

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from utils.model import users
-from utils.controller import get_user_info
+from utils.controller import get_user_info, add_user, remove_user
 
 
 def main():
@@ -15,7 +15,7 @@ def main():
         if choice == "0": break
         if choice == "1": get_user_info(users)
         if choice == "2": add_user(users)
-        if cheice == "3": remove_user(users)
+        if choice == "3": remove_user(users)
 
 
 if __name__ == "__main__":
