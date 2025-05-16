4faf5f8d4e9c782fec1e7a7b779bf9a09361656a
mordylka420 <juliemordyl@gmail.com>
2025-05-16 09:26:05 +0200

    wip:tk appgui

--- utils/model.py
+++
@@ -1,6 +0,0 @@
-users:list = [
-    {"name":"Julia","location":"Białobrzegi","posts":400},
-    {"name":"Krzysztof","location":"Białobrzegi","posts":500},
-    {"name":"Maja","location":"Swiecie","posts":300},
-    {"name":"Zuzanna","location":"Białobrzegi","posts":700},
-]
